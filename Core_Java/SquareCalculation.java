5281d404f10024d3d0b026a1a13b0aa92421b5cf
SurajOne8 <ysuraj18333@gmail.com>
2024-10-23 17:49:16 +0530

    Added new directory

---
+++ Core_Java/SquareCalculation.java
@@ -0,0 +1,20 @@
+package Anudeep_Java.Core_Java;
+
+import java.util.Scanner;
+
+    public class SquareCalculation {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter the side length of the square: ");
+            double side = scanner.nextDouble();
+
+            double area = side * side;
+            double perimeter = 4 * side;
+
+            System.out.println("Area of the square: " + area);
+            System.out.println("Perimeter of the square: " + perimeter);
+
+            scanner.close();
+    }
+}
