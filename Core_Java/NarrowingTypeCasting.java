a806499e29229df17f301970ccc50e3e77907883
SurajOne8 <ysuraj18333@gmail.com>
2024-10-23 18:02:00 +0530

    made changes in file

--- Core_Java/NarrowingTypeCasting.java
+++ Core_Java/NarrowingTypeCasting.java
@@ -1,12 +1,9 @@
 public class NarrowingTypeCasting {
     public static void main(String[] args) {
-        // Widening (automatic) type casting: int to double
         double myDouble = 9.78;
 
-        // Narrowing (manual) type casting: double to int
-        int myInt = (int) myDouble; // Explicit casting
+        int myInt = (int) myDouble;
 
-        // Output: Before and After Type Casting
         System.out.println("Original double value: " + myDouble);
         System.out.println("Narrowed int value: " + myInt);
     }
